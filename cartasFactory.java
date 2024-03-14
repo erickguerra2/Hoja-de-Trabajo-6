ce3167cc45a11d38edfe9900d0306b3ebb28ac70
erickguerra2 <eagi578@gmail.com>
2024-03-13 19:51:02 -0600

    Clase Factory

--- cartasFactory.java
+++ cartasFactory.java
@@ -1,3 +1,27 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class cartasFactory {
-    
+    public static manejadorCartas createmanejadorCartas(int choice) {
+        Map<String, String> cardsMap;
+        switch (choice) {
+            case 1:
+                cardsMap = new HashMap<>();
+                break;
+            case 2:
+                cardsMap = new TreeMap<>();
+                break;
+            case 3:
+                cardsMap = new LinkedHashMap<>();
+                break;
+            default:
+                System.out.println("Opción no válida. Se utilizará HashMap por defecto.");
+                cardsMap = new HashMap<>();
+                break;
+        }
+        return new manejadorCartas(cardsMap);
+    }
 }
+
